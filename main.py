1040d222f029f41b05fd0001c9c3ab1019e4dbfa
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-04-02 14:11:20 -0400

    Finish lesson 2, broken missing code from Lesson 4

--- main.py
+++ main.py
@@ -3,18 +3,31 @@
 # where the first '<a href=' occurs in a page.
 
 # WEB CRAWLER 
-# LESSON 1
+# LESSON 2
 
 # Page contents provided by udacity instructor
-page = '''<div id="top_bin"> <div id="top_content" class="width960">
-   <div class="udacity float-left"> <a href="/">'''
+page = '''<div id="top_bin"> <div id="top_content" class="width960"><div class="udacity float-left"> <a href="/">'''
 
 # start_link contains the value of the first position on the page that contains a link
-start_link = page.find('<a href=')
-start_quote = page.find('"',start_link)
-end_quote = page.find('"',start_quote + 1)
-url = page[start_quote+1:end_quote]
+def get_next_target(S):
+    start_link = S.find('<a href=')
+    if start_link == -1:
+        return None, 0
+    start_quote = S.find('"',start_link)
+    end_quote = S.find('"',start_quote + 1)
+    url = S[start_quote+1:end_quote]
+    return url, end_quote
 
+def print_all_links(S):
+    while True:
+        url, endpos = get_next_target(S)
+        if url:
+            print url
+            S = S[endpos:]
+        else:
+            break
 
 # Print results of start_link
-print url
+# get_page defined in Lesson 4
+print print_all_links(get_page('http://xkcd.com/353'))
+
